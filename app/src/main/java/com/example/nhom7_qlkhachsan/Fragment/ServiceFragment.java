1d09cd48fcdb1f48cd231d9ba0fb50efb55d82fd
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2022-11-07 10:30:37 +0700

    11/7/2022

---
+++ app/src/main/java/com/example/nhom7_qlkhachsan/Fragment/ServiceFragment.java
@@ -0,0 +1,27 @@
+package com.example.nhom7_qlkhachsan.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nhom7_qlkhachsan.R;
+
+
+public class ServiceFragment extends Fragment {
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_service, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/nhom7_qlkhachsan/MainActivity.java
+++ app/src/main/java/com/example/nhom7_qlkhachsan/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.nhom7_qlkhachsan;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.example.nhom7_qlkhachsan.Activity.LoginActivity;
+import com.example.nhom7_qlkhachsan.Fragment.ServiceFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +30,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.DrawerLayout);
+        frameLayout = findViewById(R.id.frameLayout);
+        toolBar = findViewById(R.id.toolBar);
+        navigationView = findViewById(R.id.navigationView);
+        headerLayout = navigationView.getHeaderView(0);
+        TextView tvTen = headerLayout.findViewById(R.id.tvTen);
+        setSupportActionBar(toolBar);
+        ActionBar actionBar =  getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment=null;
+                switch (item.getItemId()){
+                    case R.id.mDichVu:
+                        fragment = new ServiceFragment();
+                        break;
+//                    case R.id.mQLLoaiSach:
+//                        fragment = new QLLoaiSachFragment();
+//                        break;
+//                    case R.id.mQLSach:
+//                        fragment = new QLSachFragment();
+//                        break;
+//                    case R.id.mQLThanhVien:
+//                        fragment = new QLThanhVienFragment();
+//                        break;
+//                    case R.id.mTop10:
+//                        fragment = new Top10Fragment();
+//                        break;
+//                    case R.id.mDoanhThu:
+//                        fragment = new DoanhThuFragment();
+//                        break;
+//                    case R.id.mDoiMatKhau:
+//                        showDialogChangePass();
+//                        break;
+                    case R.id.mDangXuat:
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+
+                    default:
+                        fragment = new ServiceFragment();
+                        break;
+                }
+                if (fragment!=null)
+                {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, fragment)
+                            .commit();
+                    toolBar.setTitle(item.getTitle());
+                }
+
+
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return false;
+            }
+        });
 
 
     }
