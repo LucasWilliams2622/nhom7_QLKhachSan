0c98b7acfbdccc67a7bb7300f9a35f8bb8f47b0d
DZViet <117284050+DZViet@users.noreply.github.com>
2022-11-09 00:46:30 +0700

    viet 9/11 12:46 am

--- app/src/main/java/com/example/nhom7_qlkhachsan/Adapter/BookedRoomAdapter.java
+++ app/src/main/java/com/example/nhom7_qlkhachsan/Adapter/BookedRoomAdapter.java
@@ -1,4 +1,69 @@
 package com.example.nhom7_qlkhachsan.Adapter;
 
-public class BookedRoomAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nhom7_qlkhachsan.Model.AppRoom;
+import com.example.nhom7_qlkhachsan.R;
+
+import java.util.ArrayList;
+
+public class BookedRoomAdapter extends RecyclerView.Adapter<BookedRoomAdapter.ViewHolder>{
+
+    private Context context;
+    private ArrayList<AppRoom> list;
+
+    public BookedRoomAdapter(Context context, ArrayList<AppRoom> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+        View view = inflater.inflate(R.layout.item_recycler_booked_room,parent,false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+    EditText txtEndDay,txtStartDay;
+    Button btnHuyPhong;
+    TextView tvTenPhong,tvMaPhong,tvLoaiPhong,tvGiaPhong;
+    ImageView ivRoom;
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtEndDay = itemView.findViewById(R.id.txtEndDay);
+            txtStartDay = itemView.findViewById(R.id.txtStartDay);
+            btnHuyPhong = itemView.findViewById(R.id.btnHuyPhong);
+            tvTenPhong = itemView.findViewById(R.id.tvTenPhong);
+            tvMaPhong = itemView.findViewById(R.id.tvMaPhong);
+            tvLoaiPhong = itemView.findViewById(R.id.tvLoaiPhong);
+            tvGiaPhong = itemView.findViewById(R.id.tvGiaPhong);
+            ivRoom = itemView.findViewById(R.id.ivRoom);
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/nhom7_qlkhachsan/Fragment/PhongDaDatFragment.java
+++ app/src/main/java/com/example/nhom7_qlkhachsan/Fragment/PhongDaDatFragment.java
@@ -3,22 +3,37 @@ package com.example.nhom7_qlkhachsan.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.nhom7_qlkhachsan.Adapter.BookedRoomAdapter;
+import com.example.nhom7_qlkhachsan.Model.AppRoom;
 import com.example.nhom7_qlkhachsan.R;
+
+import java.util.ArrayList;
 
 
 public class PhongDaDatFragment extends Fragment {
     RecyclerView recyclerBookedRoom;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_phong_da_dat, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_phong_da_dat,container,false);
+        recyclerBookedRoom = view.findViewById(R.id.recyclerBookedRoom);
+        loadData();
+        return view;
+    }
+    private  void loadData(){
+        ArrayList<AppRoom> list = new ArrayList<>();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerBookedRoom.setLayoutManager(linearLayoutManager);
+        BookedRoomAdapter bookedRoomAdapter= new BookedRoomAdapter(getContext(), list);
+        recyclerBookedRoom.setAdapter(bookedRoomAdapter);
     }
 }
